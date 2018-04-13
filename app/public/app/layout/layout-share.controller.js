26ac0f21a89d2d8cad13ec93a09ee8ec559fc31b
andrey-kirillov <andrey.kirillov.kiev@gmail.com>
2018-04-13 12:28:57 +0300

    >50 share send

--- app/public/app/layout/layout-share.controller.js
+++ app/public/app/layout/layout-share.controller.js
@@ -12,20 +12,27 @@
         vm = this;
 
         vm.shareEmails = '';
+        vm.tooMuchMails = false;
 
         vm.sendEmail = function() {
             var chartInfo = commonService.getShareChart();
             var emails = vm.shareEmails.split(',');
-            $http.post(BASE_URL + 'share/send-email', {
-                emails: emails,
-                subject: [chartInfo.title, chartInfo.type, 'Chart'].join(' '),
-                image: chartInfo.url
-            }).then(function(responce) {
-                if (responce.data === 'success') {
-                    vm.shareEmails = '';
-                    commonService.notification($scope.getTranslation('email_sent'), 'success');
-                }
-            });
-        }
+            if (emails.length >= 50) {
+                vm.tooMuchMails = true;
+                return false;
+            } else {
+                vm.tooMuchMails = false;
+                $http.post(BASE_URL + 'share/send-email', {
+                    emails: emails,
+                    subject: [chartInfo.title, chartInfo.type, 'Chart'].join(' '),
+                    image: chartInfo.url
+                }).then(function (responce) {
+                    if (responce.data === 'success') {
+                        vm.shareEmails = '';
+                        commonService.notification($scope.getTranslation('email_sent'), 'success');
+                    }
+                });
+            }
+        };
     }
 })();
