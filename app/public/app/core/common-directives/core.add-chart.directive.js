1511fd718a377392f444fa6bb45ee2ab9bc48ad9
AleaJactaEst <kachan.vitaliy12@gmail.com>
2018-06-21 16:36:01 +0300

    2247 tooltip issue

--- app/public/app/core/common-directives/core.add-chart.directive.js
+++ app/public/app/core/common-directives/core.add-chart.directive.js
@@ -5,15 +5,14 @@
     angular.module('app.core')
         .directive('addChart', addChart);
 
-    addChart.$inject = ['$compile', 'commonService'];
-    function addChart($compile, commonService) {
+    addChart.$inject = ['$compile', 'commonService', '$document'];
+    function addChart($compile, commonService, $document) {
         return {
             restrict: 'A',
             //transclude: true,
             scope: {status: '='},
             link: function ($scope, $element, $attrs) {
                 var chartFrag, chartEl;
-
                 // watch for status to change on chart item added even
                 $scope.$watch('status', function (newVal, oldVal) {
                     if (newVal) {
@@ -31,8 +30,36 @@
                         //$scope.$destroy();
                         $element.remove();
                     }
-                }, true);
+                }, true)
+
+
+                $element.parent().bind('mousemove', function () {
+                    // the check is required due to prevent effect of mousemove on chart while it is in process of rendering
+                    if($scope.$parent.w.chart_data.chart.showToolTip === '0') {
+                        $scope.$parent.w.chart_data.chart.showToolTip = '1';
+                        $scope.$apply();
+                    }
+                });
+
+                $element.parent().bind('contextmenu', function () {
+                    $scope.$parent.w.chart_data.chart.showToolTip = '0';
+                    $scope.$apply();
+                });
+
+                $document.bind('keydown', function () {
+                    $scope.$parent.w.chart_data.chart.showToolTip = '0';
+                    $scope.$apply();
+                });
+
+                $scope.$on('$destroy', function () {
+                    $document.unbind('keydown');
+                    $element.parent().unbind('contextmenu');
+                    $element.parent().unbind('mousemove');
+                });
+
             }
+
+
         };
     }
 })();
